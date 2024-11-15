7b059190be2ab24eaa55d4f18db9b2e864067717
mully93 <emmamullane93@gmail.com>
2024-11-15 14:53:13 +0000

    added api intregration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Come away o human child",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "c3tb0f348f4a0b50181cb4o12204a3db";
+  let context =
+    "You are a short poem expert. Your mission is to generate a short, four-line poem in basic HTML and separate each line with a < /br>. Set a word count of 25. Please make sure to follow the user instructions below";
+  let prompt = `User instructions: Generate a short poem about ${instructionInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context={context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
